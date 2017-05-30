6dd4e37fcd50bf2864599b61d165a6cd827fe41e
Laura Guzelj Blatnik <laura.guzelj-blatnik@student.fmf.uni-lj.si>
2017-05-30 20:33:55 +0200

    dopolnila grafični vmestnik

--- grafika2048.py
+++ grafika2048.py
@@ -1,26 +1,22 @@
 # projektna naloga
 
 import tkinter as tk
+import 2048
+
+class Plosca:
+    def __init__(self, okno):
+
+    def obdelaj_tipko(self, event):
+        if event.keysym == 'Right':
+            self.2048.stisni_desno(A)
+        elif event.keysym == 'Left':
+            self.2048.stisni_levo(A)
+        elif event.keysym == 'Up':
+            self.2048.stisni_gor(A)
+        elif event.keysym == 'Down':
+            self.2048.stisni_dol(A)
+
+
 
 okno = tk.Tk()
-
-#igra = tk.Frame(okno)
-#igra.grid(row=0, column=0)
-
-#gumbi = tk.Frame(okno)
-#gumbi.grid(row=0, column=1)
-#gor = tk.Button(gumbi, text='↑')
-#gor.grid(row=0, column=1)
-#levo = tk.Button(gumbi, text='←')
-#levo.grid(row=1, column=0)
-#dol = tk.Button(gumbi, text='↓')
-#dol.grid(row=1, column=1)
-#desno = tk.Button(gumbi, text='→')
-#desno.grid(row=1, column=2)
-
-igralna_plosca = tk.Frame(okno)
-igralna_plosca = tk.Canvas(okno, width=500, height=500)
-
-
-
 okno.mainloop()
